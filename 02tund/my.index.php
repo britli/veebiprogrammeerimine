573f8922458dd10d718b1f806626db229d66b9db
Brita Liivamaa <britli@users.noreply.github.com>
2019-09-11 14:59:50 +0300

    Esimene üleslaadimine

---
+++ 02tund/my.index.php
@@ -0,0 +1,28 @@
+<?php
+ $userName = "Brita Liivamaa";
+ $fullTimeNow = date("d.m.Y H:i:s");
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+   programmeerib veebi</title>
+</head>
+<body>
+  <?php
+   echo "<h1>" .$userName .", veebiprogrammeerimine 2019</h1>";
+    ?>
+	
+  <p>See veebileht on valminud õppetöö käigus ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+<?php
+ echo "See on minu esimene PHP!</p>";
+ echo "<p> Lehe avamise hetkel oli " .$fullTimeNow . ".</p>" ;
+?>
+
+
+</body>
+</html>
